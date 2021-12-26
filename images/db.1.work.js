7b86b1d5ddedffb28645cb3c1fc1c84f7e877770
popocreator <popomonclub@gmail.com>
2021-12-27 03:28:49 +0900

    Added post logo and page code - doing

--- images/db.1.work.js
+++ images/db.1.work.js
@@ -1,6 +1,96 @@
 const work = [
-  {title:'프로젝트'},
-  {title:'이슈'},
-  {title:'테스트'},
-  {title:'회고'}
+  {
+    catogory: "New Features",
+    order: 1,
+    date: "2022. 01. 01",
+    title: "3 Easy Ways To Make Your iPhone Faster",
+    link: "/100",
+    translation: ["ko", ""],
+    status: "done"
+  },
+  {
+    catogory: "Project's Issues",
+    order: 1,
+    date: "2022. 01. 01",
+    title: "3 Easy Ways To Make Your iPhone Faster",
+    link: "/100",
+    translation: ["ko", "en"],
+    status: "doing"
+  },
+  {
+    catogory: "CI/CD Test",
+    order: 1,
+    date: "2022. 01. 01",
+    title: "3 Easy Ways To Make Your iPhone Faster",
+    link: "/100",
+    translation: ["ko", ""],
+    status: "todo"
+  },
+  {
+    catogory: "Retrospect",
+    order: 1,
+    date: "2022. 01. 01",
+    title: "3 Easy Ways To Make Your iPhone Faster",
+    link: "/100",
+    translation: ["ko", "en"],
+    status: "todo"
+  }
 ];
+
+function renderWorksStatus() {
+  document.querySelector(".new-features .number").innerHTML = work.filter(
+    (w) => w.catogory === "New Features"
+  ).length;
+  document.querySelector(".project-issues .number").innerHTML = work.filter(
+    (w) => w.catogory === "Project's Issues"
+  ).length;
+  document.querySelector(".ci-cd-test .number").innerHTML = work.filter(
+    (w) => w.catogory === "CI/CD Test"
+  ).length;
+  document.querySelector(".retrospect .number").innerHTML = work.filter(
+    (w) => w.catogory === "Retrospect"
+  ).length;
+}
+
+function renderWorks(category) {
+  let posts = [];
+  if (category) {
+    posts = work.filter((w) => w.catogory === category);
+  } else {
+    posts = work;
+  }
+
+  let tbody = "";
+  posts.forEach((post) => {
+    let tr = "";
+    tr += "<tr>";
+    tr += '  <td class="blue left">#' + post.catogory + "</td>";
+    tr += "  <td>" + post.date + "</td>";
+    tr += '  <td class="blue">';
+    tr += '    <a href="' + post.link + '">';
+    tr += "      " + post.title + "";
+    tr += "    </a>";
+    tr += "  </td>";
+    tr += "  <td>";
+    if (post.translation.includes("ko")) {
+      tr += '    <span class="tag green">KO</span>';
+    }
+    if (post.translation.includes("en")) {
+      tr += '    <span class="tag pink">EN</span>';
+    }
+    tr += "  </td>";
+    tr += '  <td class="center">';
+    if (post.status === "done") {
+      tr += '    <span class="tag green">DONE</span>';
+    } else if (post.status === "doing") {
+      tr += '    <span class="tag yellow">DOING</span>';
+    } else {
+      tr += '    <span class="tag pink">TODO</span>';
+    }
+    tr += "  </td>";
+    tr += "</tr>";
+    tbody += tr;
+  });
+
+  document.querySelector("#post-table tbody").innerHTML = tbody;
+}
--- images/script.js
+++ images/script.js
@@ -1,19 +1,83 @@
-document.querySelectorAll('.category.disabled')
-  .forEach(disabled => {
-    disabled.addEventListener('click', () => {
-      alert('Coming soon');
-    });
-  });
-
-function loading(){
+function loading() {
   setTimeout(() => {
-    document.getElementById('loading').style.display = 'none';
+    document.getElementById("loading").style.display = "none";
   }, 2000);
   setTimeout(() => {
-    document.getElementById('drawer').style.transform = 'translateX(0%)';
+    document.getElementById("drawer").style.transform = "translateX(0%)";
   }, 3000);
 }
 
-window.onload = function(){
-  loading();
+function commingSoon() {
+  document.querySelectorAll(".category.disabled").forEach((disabled) => {
+    disabled.addEventListener("click", () => {
+      alert("Comming Soon");
+    });
+  });
 }
+
+function bodyCheck() {
+  const HOME = "tt-body-index";
+  const POST_LIST = "tt-body-page";
+  const CATEGORY_LIST = "tt-body-category";
+  const SEARCH_LIST = "tt-body-search";
+  const GUESTBOOK_LIST = "tt-body-guestbook";
+
+  const bodyType = document.querySelector("#body").dataset.type;
+  switch (bodyType) {
+    case HOME:
+      document.querySelector("#drawer").style.display = "flex";
+      document.querySelector("#contents").style.display = "flex";
+      break;
+    case POST:
+      document.querySelector("#drawer").style.display = "none";
+      document.querySelector("#contents").style.display = "flex";
+      break;
+    case CATEGORY_LIST:
+      document.querySelector("#drawer").style.display = "flex";
+      document.querySelector("#contents").style.display = "flex";
+      break;
+    case SEARCH_LIST:
+      document.querySelector("#drawer").style.display = "flex";
+      document.querySelector("#contents").style.display = "flex";
+      break;
+    case GUESTBOOK_LIST:
+      document.querySelector("#drawer").style.display = "none";
+      document.querySelector("#contents").style.display = "none";
+      break;
+    default:
+      break;
+  }
+}
+
+function setNow() {
+  const now = moment().format("LL");
+  document.querySelector(".now .date").innerHTML = now;
+}
+
+function init() {
+  loading();
+  commingSoon();
+  bodyCheck();
+
+  // Works
+  renderWorksStatus();
+  renderWorks();
+}
+
+window.onload = function () {
+  setNow();
+  document.getElementById("loading").style.display = "none";
+  // init();
+  renderWorksStatus();
+  renderWorks();
+};
+
+// 페이지 타입	body_id (data-body-type)
+// 홈화면	tt-body-index
+// 글화면	tt-body-page
+// 카테고리 글 리스트	tt-body-category
+// 보관함 글 리스트	tt-body-archive
+// 태그 리스트	tt-body-tag
+// 검색결과 리스트	tt-body-search
+// 방명록	tt-body-guestbook
+// 지역로그	tt-body-location
