82b279e7e11f1e1d175b0a858a6393882838fee2
popocreator <popomonclub@gmail.com>
2022-01-03 03:24:13 +0900

    Added mobile dashboard style

--- images/db.1.work.js
+++ images/db.1.work.js
@@ -7,6 +7,33 @@ const work = [
     link: "/39",
     translation: ["ko", ""],
     status: "done"
+  },
+  {
+    catogory: "New Features",
+    order: 1,
+    date: "2022. 01. 03",
+    title: "스크롤을 내리면, 사라지는 헤더",
+    link: "/43",
+    translation: ["ko", ""],
+    status: "todo"
+  },
+  {
+    catogory: "New Features",
+    order: 1,
+    date: "2022. 01. 03",
+    title: "멀티 드래그 리스트",
+    link: "/42",
+    translation: ["ko", ""],
+    status: "todo"
+  },
+  {
+    catogory: "New Features",
+    order: 1,
+    date: "2022. 01. 03",
+    title: "가이드 모달",
+    link: "/40",
+    translation: ["ko", ""],
+    status: "todo"
   }
 ];
 
